cf493ecbce853fdef9c5ad56e1bcc3f109ff33c5
David <dvdplm@gmail.com>
2021-01-19 18:28:56 +0000

    A bit more idiomatic rust example (#19)
    * A bit more idiomatic rust
    
    * Update substrate-airgapped/Cargo.toml
    
    Co-authored-by: Zeke Mostov <32168567+emostov@users.noreply.github.com>
    
    * Update substrate-airgapped/Cargo.toml
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * Update substrate-airgapped/Cargo.toml
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * Update substrate-airgapped/examples/signed_tx_from_pair.rs
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * Update substrate-airgapped/examples/signed_tx_from_pair.rs
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * review feedback
    
    * review feedback
    
    * Ditch anyhow
    
    Co-authored-by: Zeke Mostov <32168567+emostov@users.noreply.github.com>
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

--- substrate-airgapped/examples/signed_tx_from_pair.rs
+++ substrate-airgapped/examples/signed_tx_from_pair.rs
@@ -1,5 +1,5 @@
 use codec::Encode;
-use sp_core::H256;
+use primitive_types::H256;
 use sp_runtime::{generic::Header, traits::BlakeTwo256, DeserializeOwned};
 use substrate_airgapped::{
 	balances::Transfer, CallIndex, GenericCall, KusamaRuntime, MortalConfig, Mortality, Tx,
@@ -12,38 +12,34 @@ use reqwest;
 use serde::{Deserialize, Serialize};
 use sp_keyring::AccountKeyring;
 use sp_version::RuntimeVersion;
+use std::str::FromStr;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+type KusamaTransfer = Transfer<KusamaRuntime>;
+type Error = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), Error> {
 	// Get the latest block hash and then make all non historic queries at that block.
-	let block_hash = rpc_to_local_node::<(), String>("chain_getBlockHash", vec![])?.result;
-	let runtime_version = rpc_to_local_node::<String, RuntimeVersion>(
-		"chain_getRuntimeVersion",
-		vec![block_hash.clone()],
-	)?
-	.result;
-	let header = rpc_to_local_node::<String, Header<u32, BlakeTwo256>>(
-		"chain_getHeader",
-		vec![block_hash.clone()],
-	)?
-	.result;
-	let genesis_hash = rpc_to_local_node::<usize, String>("chain_getBlockHash", vec![0])
-		.and_then(|rpc_res| Ok(string_to_h256(&rpc_res.result)))?;
-	let block_hash = string_to_h256(&block_hash[..]);
+	let block_hash: H256 = rpc_to_local_node("chain_getBlockHash", Vec::<()>::new())?.result;
+	let runtime_version: RuntimeVersion =
+		rpc_to_local_node("chain_getRuntimeVersion", vec![block_hash])?.result;
+	let header: Header<u32, BlakeTwo256> =
+		rpc_to_local_node("chain_getHeader", vec![block_hash.clone()])?.result;
 
-	type TransferType = Transfer<KusamaRuntime>;
+	let genesis_hash: H256 = rpc_to_local_node("chain_getBlockHash", vec![0])
+		.map(|rpc_res: RpcRes<String>| H256::from_str(&rpc_res.result[2..]))??;
 
 	let alice_addr = AccountKeyring::Alice.to_account_id().into();
 	let bob_addr = AccountKeyring::Bob.to_account_id().into();
-	let transfer_call =
-		GenericCall::new(CallIndex::new(5, 0), Transfer { to: bob_addr, amount: 123_456 });
+	let call =
+		GenericCall::new(CallIndex::new(5, 0), KusamaTransfer { to: bob_addr, amount: 123_456 });
 
-	let tx: Tx<TransferType, KusamaRuntime> = Tx::new(TxConfig {
-		call: transfer_call,
+	let tx: Tx<KusamaTransfer, KusamaRuntime> = Tx::new(TxConfig {
+		call,
 		address: alice_addr,
 		nonce: 0,
 		tx_version: runtime_version.transaction_version,
 		spec_version: runtime_version.spec_version,
-		genesis_hash: genesis_hash,
+		genesis_hash,
 		mortality: Mortality::Mortal(MortalConfig {
 			period: 64,
 			checkpoint_block_number: header.number as u64,
@@ -71,10 +67,10 @@ struct RpcRes<T> {
 
 /// RPC request JSON object
 #[derive(Serialize, Deserialize)]
-struct RpcReq<T: Serialize> {
-	jsonrpc: String,
+struct RpcReq<'a, T: Serialize> {
+	jsonrpc: &'a str,
 	id: usize,
-	method: String,
+	method: &'a str,
 	params: Vec<T>,
 }
 
@@ -82,23 +78,18 @@ struct RpcReq<T: Serialize> {
 fn rpc_to_local_node<T: Serialize, U: DeserializeOwned>(
 	method: &str,
 	params: Vec<T>,
-) -> Result<RpcRes<U>, Box<dyn std::error::Error>> {
-	let local_node_url = "http://localhost:9933";
+) -> Result<RpcRes<U>, Error> {
+	const LOCAL_NODE_URL: &'static str = "http://localhost:9933";
+	const TWO_ZERO: &'static str = "2.0";
 	let client = reqwest::blocking::Client::new();
 
-	let req_body = RpcReq { jsonrpc: "2.0".to_owned(), id: 1, method: method.to_owned(), params };
-	let res = client.post(local_node_url).json(&req_body).send()?.json()?;
-
-	Ok(res)
-}
-
-/// Convert a hex string to `H256`
-fn string_to_h256(value: &str) -> H256 {
-	// Slice of the "0x" prefix
-	let no_prefix = &value[2..];
-	let bytes = hex::decode(no_prefix).expect("only valid hex strings are passed in");
-
-	H256::from_slice(bytes.as_slice())
+	let req_body = RpcReq { jsonrpc: TWO_ZERO, id: 1, method, params };
+	client
+		.post(LOCAL_NODE_URL)
+		.json(&req_body)
+		.send()?
+		.json()
+		.map_err(Into::into)
 }
 
 // The below may be useful for those reading from file on an offline device... not sure where to put them
@@ -116,7 +107,7 @@ use std::path::PathBuf;
 ///
 /// where `result` is a field representing scale encoded bytes.
 #[allow(dead_code)]
-fn rpc_to_bytes(path: PathBuf) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+fn rpc_to_bytes(path: PathBuf) -> Result<Vec<u8>, Error> {
 	let contents = file_to_string(path)?;
 
 	let rpc_response: RpcRes<String> = serde_json::from_str(&contents)?;
@@ -139,7 +130,7 @@ fn rpc_to_bytes(path: PathBuf) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 ///
 /// where `result` is a field representing a struct in JSON.
 #[allow(dead_code)]
-fn rpc_to<T: DeserializeOwned>(path: PathBuf) -> Result<T, Box<dyn std::error::Error>> {
+fn rpc_to<T: DeserializeOwned>(path: PathBuf) -> Result<T, Error> {
 	let contents = file_to_string(path)?;
 
 	let rpc_response: RpcRes<T> = serde_json::from_str(&contents)?;
@@ -149,7 +140,7 @@ fn rpc_to<T: DeserializeOwned>(path: PathBuf) -> Result<T, Box<dyn std::error::E
 
 /// Read a file to a string (non-buffered).
 #[allow(dead_code)]
-fn file_to_string(path: PathBuf) -> Result<String, Box<dyn std::error::Error>> {
+fn file_to_string(path: PathBuf) -> Result<String, Error> {
 	let mut file = File::open(path)?;
 	let mut contents = String::new();
 	file.read_to_string(&mut contents)?;
